92100a539224dc5d91db74285aa608bf6fd8a2c5
Jim Tryon <jtryon@mail.greenriver.edu>
2015-04-25 10:48:49 -0700

    Added main feed

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Comment Box</title>
+        <link href="css/layout.css" rel="stylesheet" />
+        <script src="js/query.js"></script>
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+        
+        <!-- start wrapper -->
+        <div class="wrapper">
+            <div class="page-data">
+                Page content is in here
+            </div>
+            <div class="comment-wrapper">
+                
+                <h3 class="comment-title">News Feed</h3>
+                
+                <div class="comments-list">
+                    
+                    <ul class="comments-holder-ul">
+                        <li class="comment-holder" id="_1">
+                            <div class="user-img">
+                                <img src="img/img-placeholder.png" class="user-img-pic" />
+                            </div>
+                            
+                            <div class="comment-body">
+                                <h3 class="username-field">
+                                Jim Tryon
+                                </h3>
+                                <div class="comment-text">
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+
+                                </div>
+                            </div>
+
+                        </li>
+                        <li class="comment-holder" id="_1">
+                        <div class="user-img">
+                                <img src="img/img-placeholder.png" class="user-img-pic" />
+                            </div>
+                            
+                            <div class="comment-body">
+                                <h3 class="username-field">
+                                Mike Lant
+                                </h3>
+                                <div class="comment-text">
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+                                    Posts will go here<br />
+
+                                </div>
+                            </div>
+
+                        </li>
+                        </ul>
+                    </div>
+                </div>
+                </div> <!-- end wrapper -->
+            </body>
+        </html>
