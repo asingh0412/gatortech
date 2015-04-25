8066e16d0e1db040d602bb23c902c0187507f256
Jim Tryon <jtryon@mail.greenriver.edu>
2015-04-25 13:24:16 -0700

    Added bootstrap layout

--- index.php
+++ index.php
@@ -1,79 +1,80 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <meta charset="UTF-8">
         <title>Comment Box</title>
-        <link href="css/layout.css" rel="stylesheet" />
-        <script src="js/query.js"></script>
-        <script src="js/script.js"></script>
+        
+        <link rel="stylesheet" type="text/css" media="all" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" media="all" href="css/layout.css">
+        <script type="text/javascript" src="js/query.js"></script>
+        <script type="text/javascript"  charset="utf-8" src="js/bootstrap.min.js"></script>
     </head>
     <body>
         
         <!-- Center the page in the middle and contain the layout
         to a specific width -->
-        <div class="wrapper"> <!-- start wrapper -->
-            <div class="page-data">
-                Page content is in here
-            </div>
-            <div class="comment-wrapper">
+        <div class="container"> <!-- start wrapper -->
+        <div class="page-data">
+            Page content is in here
+        </div>
+        <div class="comment-wrapper">
+            
+            <h3 class="comment-title">News Feed</h3>
+            
+            <div class="comments-list">
                 
-                <h3 class="comment-title">News Feed</h3>
-                
-                <div class="comments-list">
-                    
-                    <ul class="comments-holder-ul">
-                        <li class="comment-holder" id="_1">
-                            <div class="user-img">
-                                <img src="img/img-placeholder.png" class="user-img-pic" />
-                            </div>
-                            
-                            <div class="comment-body">
-                                <h3 class="username-field">
-                                Jim Tryon
-                                </h3>
-                                <div class="comment-text">
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-
-                                </div>
-                            </div>
-
-                        </li>
-                        <li class="comment-holder" id="_1">
+                <ul class="comments-holder-ul">
+                    <li class="comment-holder" id="_1">
                         <div class="user-img">
-                                <img src="img/img-placeholder.png" class="user-img-pic" />
+                            <img src="img/img-placeholder.png" class="user-img-pic"
+                            alt="Placeholder for image" />
+                        </div>
+                        
+                        <div class="comment-body">
+                            <h3 class="username-field">
+                            Jim Tryon
+                            </h3>
+                            <div class="comment-text">
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
                             </div>
-                            
-                            <div class="comment-body">
-                                <h3 class="username-field">
-                                Mike Lant
-                                </h3>
-                                <div class="comment-text">
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-
-                                </div>
+                        </div>
+                    </li>
+                    <li class="comment-holder" id="_2">
+                        <div class="user-img">
+                            <img src="img/img-placeholder.png" class="user-img-pic"
+                            alt="Placeholder for image" />
+                        </div>
+                        
+                        <div class="comment-body">
+                            <h3 class="username-field">
+                            Mike Lant
+                            </h3>
+                            <div class="comment-text">
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
+                                Posts will go here<br />
                             </div>
-
-                        </li>
-                        </ul>
-                    </div>
-                </div>
-                </div> <!-- end wrapper -->
-            </body>
-        </html>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </div> <!-- end wrapper -->
+    </body>
+</html>
