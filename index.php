6c742267b076120ea01aa61a5485925054cf8a02
Jim Tryon <jtryon@mail.greenriver.edu>
2015-04-26 04:56:41 -0700

    Added lorem filler text and responsive images

--- index.php
+++ index.php
@@ -1,82 +1,95 @@
 <!DOCTYPE html>
 <html>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <meta charset="utf-8">
-            <title>Comment Box</title>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <title>Comment Box</title>
+        
+        <link rel="stylesheet" type="text/css" media="all" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" media="all" href="css/layout.css">
+        <script type="text/javascript" src="js/query.js"></script>
+        <script type="text/javascript"  charset="utf-8" src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        
+        <!-- Center the page in the middle and contain the layout
+        to a specific width -->
+        <div class="comment-container"> <!-- start wrapper -->
+        <div class="page-data">
+            Page content is in here
+        </div>
+        <h3 class="comment-title">News Feed</h3>
+        
+        <div class="comments-list">
             
-            <link rel="stylesheet" type="text/css" media="all" href="css/bootstrap.min.css">
-            <link rel="stylesheet" type="text/css" media="all" href="css/layout.css">
-            <script type="text/javascript" src="js/query.js"></script>
-            <script type="text/javascript"  charset="utf-8" src="js/bootstrap.min.js"></script>
-        </head>
-        <body>
-            
-            <!-- Center the page in the middle and contain the layout
-            to a specific width -->
-        <div class="container"> <!-- start wrapper -->
-
-            <div class="page-data">
-                Page content is in here
-            </div>
-            <div class="comment-wrapper">
-                
-                <h3 class="comment-title">News Feed</h3>
-                
-                <div class="comments-list">
+            <ul class="comments-holder-ul">
+                <li class="comment-holder" id="_1">
+                    <div class="user-img">
+                        <img src="img/placeholder.jpeg"
+                        class="user-img-pic img-responsive"
+                        alt="Placeholder for image" />
+                    </div>
                     
-                    <ul class="comments-holder-ul">
-                        <li class="comment-holder" id="_1">
-                            <div class="user-img">
-                                <img src="img/img-placeholder.png" class="user-img-pic"
-                                alt="Placeholder for image" />
-                            </div>
-                            
-                            <div class="comment-body">
-                                <h3 class="username-field">
-                                Jim Tryon
-                                </h3>
-                                <div class="comment-text">
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                </div>
-                            </div>
-                        </li>
-                        <li class="comment-holder" id="_2">
-                            <div class="user-img">
-                                <img src="img/img-placeholder.png" class="user-img-pic"
-                                alt="Placeholder for image" />
-                            </div>
-                            
-                            <div class="comment-body">
-                                <h3 class="username-field">
-                                Mike Lant
-                                </h3>
-                                <div class="comment-text">
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                    Posts will go here<br />
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div> <!-- end wrapper -->
-    </body>
+                    <div class="comment-body">
+                        <h3 class="username-field">
+                        Jim
+                        </h3>
+                        <div class="comment-text">
+                            Proin pellentesque a sem sed feugiat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse eget.
+                        </div>
+                    </div>
+                </li>
+                <li class="comment-holder" id="_2">
+                    <div class="user-img">
+                        <img src="img/placeholder.jpeg"
+                        class="user-img-pic img-responsive"
+                        alt="Placeholder for image" />
+                    </div>
+                    
+                    <div class="comment-body">
+                        <h3 class="username-field">
+                        Mike
+                        </h3>
+                        <div class="comment-text">
+                           Ut sollicitudin, nisi quis ullamcorper aliquet, tortor justo rhoncus dui, eu facilisis urna ante id augue.
+                        </div>
+                    </div>
+                </li>
+                <li class="comment-holder" id="_3">
+                    <div class="user-img">
+                        <img src="img/placeholder.jpeg"
+                        class="user-img-pic img-responsive"
+                        alt="Placeholder for image" />
+                    </div>
+                    
+                    <div class="comment-body">
+                        <h3 class="username-field">
+                        Aman
+                        </h3>
+                        <div class="comment-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae molestie felis.  
+                        </div>
+                    </div>
+                </li>
+                <li class="comment-holder" id="_4">
+                    <div class="user-img">
+                        <img src="img/placeholder.jpeg"
+                        class="user-img-pic img-responsive"
+                        alt="Placeholder for image" />
+                    </div>
+                    
+                    <div class="comment-body">
+                        <h3 class="username-field">
+                        Nick
+                        </h3>
+                        <div class="comment-text">
+                            Vestibulum vehicula feugiat mauris
+                        </div>
+                    </div>
+                </li> 
+            </ul>
+        </div>
+    </div>
+    </div> <!-- end wrapper -->
+</body>
 </html>
