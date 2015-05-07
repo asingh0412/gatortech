8f22ef48e9b2aa306d2fadbb36686789945b4e10
Michael <michael.lant@ymail.com>
2015-05-06 20:45:28 -0700

    heavy mod of html on feed page, still working

--- index.php
+++ index.php
@@ -33,69 +33,34 @@
         <title>Feed</title>
 
         <link rel="stylesheet" type="text/css" media="all" href="css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" media="all" href="css/layout.css">
-            <link href="css/nav.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" media="all" href="css/index.css">
         <script type="text/javascript" src="js/query.js"></script>
-        <script type="text/javascript" src="js/nav.js"></script>
         <script type="text/javascript"  charset="utf-8" src="js/bootstrap.min.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <!-- Nav bar jquery will move in future. -->
-        <script>
-            $(function() {
-                var pull        = $('#pull');
-                    menu        = $('nav ul');
-                    menuHeight  = menu.height();
-
-                $(pull).on('click', function(e) {
-                    e.preventDefault();
-                    menu.slideToggle();
-                });
-            });
-
-            $(window).resize(function(){
-                var w = $(window).width();
-                if(w > 320 && menu.is(':hidden')) {
-                    menu.removeAttr('style');
-                }
-            });
-        </script>
     </head>
     <body>
-       <nav class="clearfix">
-        <ul class="clearfix">
-            <li><a href="#">Home</a></li>
-            <li><a href="index.php">Feed</a></li>
-            <li><a href="#">Profile</a></li>
-         <!--   <li><a href="#">Design</a></li>
-            <li><a href="#">Web 2.0</a></li>
-            <li><a href="#">Tools</a></li>  -->
-            <a style="float: right" href='logout.php'>log out</a>
-
-             <!-- Nav bar Image styled as button -->
-            <button type="button"  class="btn btn-circle btn-xl">image</button>
-        </ul>
-            <a href="#" id="pull">Menu</a>
-       </nav>
-
-
+        <div id="site-container">
+        <div class="container-fluid" id="nav">
+	    <div id='profile-bubble'>
+                <a href='signout.php'> Sign Out </a>
+		<img src='img/Headshot1.jpg' id='profile-picture-sm'>
+	    </div>
+        </div>
+        
         <!-- Center the page in the middle and contain the layout
         to a specific width -->
-        <div class="container"> <!-- start wrapper -->
-
-         <form method="post" action="feed-post.php">
-            <input type="text" name="message" size="50" maxlength="400">
-            <input type="submit" name="post" value="Post">
-        </form>
-
-        <div class="page-data">
-
-        </div>
+        <div id="page-content" class="container-fluid"> <!-- start wrapper -->
+            <form method="post" action="feed-post.php">
+                <input type="text" name="message" size="50" maxlength="400">
+                <input type="submit" name="post" value="Post">
+            </form>
+            <div class="page-data">
+            </div>
        
-        <h3 class="comment-title">News Feed</h3>
+            <h3 class="comment-title">News Feed</h3>
 
-        <div class="comments-list">
-
-            <ul class="comments-holder-ul">
+            <div class="comments-list">
+                <ul class="comments-holder-ul">
                 <?php foreach($result as $row) {
                     echo "<li class='comment-holder' id='_1'>
                             <div class='user-img'>
@@ -119,10 +84,11 @@
                             </div>
                         </li>";
                         }
-        ?>
+                ?>
             </ul>
         </div>
     </div>
     </div> <!-- end wrapper -->
+    </div>
 </body>
 </html>
