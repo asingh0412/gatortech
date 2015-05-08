19ef0f1320cb8e0f3b4058f300d753c1987b9a81
Jim Tryon <jtryon@Jims-MacBook-Pro.local>
2015-05-07 17:52:09 -0700

    Added changed files

---
+++ demo.php
@@ -0,0 +1,20 @@
+<div id="addCommentContainer">
+    <p>Add a Comment</p>
+    <form id="addCommentForm" method="post" action="">
+      <div>
+          <label for="name">Your Name</label>
+          <input type="text" name="name" id="name" />
+
+            <label for="email">Your Email</label>
+            <input type="text" name="email" id="email" />
+
+            <label for="url">Website (not required)</label>
+            <input type="text" name="url" id="url" />
+
+            <label for="body">Comment Body</label>
+            <textarea name="body" id="body" cols="20" rows="5"></textarea>
+
+            <input type="submit" id="submit" value="Submit" />
+        </div>
+    </form>
+</div>
