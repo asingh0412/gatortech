cdec0a5e3b1cce01f2351f574a3b79cd79777d7c
Michael <michael.lant@ymail.com>
2015-05-10 23:32:01 -0700

    another file bites the dust

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['login_session']);
-session_destroy();
-header("location: login.php");
-echo 'You have been logged out. <a href="login.php">Go back</a>';
-
-?>
