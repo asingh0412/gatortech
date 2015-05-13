e4570e457c394a99ee416272acd45a4de7a404d3
Nick <tollettn@gmail.com>
2015-05-13 00:35:20 -0700

    Ok, now images work maybe?  I don't know, man
    Let's hope it works

--- feed_post.php
+++ feed_post.php
@@ -11,7 +11,7 @@
             $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
     
             // Login account verification
-            $sql = $dbh->prepare("SELECT name FROM account WHERE email = '$_SESSION[login_session]'");
+            $sql = $dbh->prepare("SELECT name, picture FROM account WHERE email = '$_SESSION[login_session]'");
             $sql->execute();
             $result = $sql->fetch();
     
@@ -19,11 +19,13 @@
     
             $data = $_POST['serialize'];
             $name = $result['name'];
+            $picture = $result['picure'];
     
-            $stmt = $dbh->prepare("INSERT INTO feed (user_email, user_name, user_post) VALUES (:user_email, :user_name, :user_post)");
+            $stmt = $dbh->prepare("INSERT INTO feed (user_email, user_name, user_post, picture) VALUES (:user_email, :user_name, :user_post, :picture)");
             $stmt->bindParam(':user_name', $name);
             $stmt->bindParam(':user_email', $_SESSION['login_session']);
             $stmt->bindParam(':user_post', $_POST['message']);
+            $stmt->bindParam(':picture', $picture);
             $stmt->execute();
     
             header("Location: index.php");
