1a481fb01e7ea781fb6b732c0e2059ce539d0f56
Jim Tryon <jtryon@Jims-MBP.PK5001Z>
2015-05-13 02:09:32 -0700

    Removed extra comment

--- login.php
+++ login.php
@@ -57,7 +57,7 @@
 	</div> <!-- Account Wall -->
       </div> <!-- Container Fluid / Login Container -->
     </div> <!-- Site Container
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+     jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
