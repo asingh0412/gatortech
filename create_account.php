ec89fec32147ab62ecca40889b04d34243d7466b
Jim Tryon <tryonjf@gmail.com>
2015-05-21 13:42:23 -0700

    Added email, password fields.  Established db connection

--- create_account.php
+++ create_account.php
@@ -1,5 +1,46 @@
 <?php
 
+include('includes/session.php');
+require "includes/db.php";
+
+// Form handling
+$name = $_POST['name'];
+$email = $_POST['email'];
+$form_password = $_POST['password'];
+$prog = $_POST['program'];
+$_date = $_POST['datepicker'];
+
+$vars = array('name', 'email', 'password', 'program', 'datepicker',);
+
+foreach($vars as $var_name)
+{
+
+   if(!empty($_POST[$var_name])) 
+   {
+        
+    }
+}
+
+try
+        {
+            // New database connection
+            $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
+    
+            // $stmt = $dbh->prepare("INSERT INTO feed (user_email, user_name, user_post, picture) VALUES (:user_email, :user_name, :user_post, :picture)");
+            // $stmt->bindParam(':user_name', $name);
+            // $stmt->bindParam(':user_email', $_SESSION['login_session']);
+            // $stmt->bindParam(':user_post', $_POST['message']);
+            // $stmt->bindParam(':picture', $picture);
+            // $stmt->execute();
+    
+            //header("Location: index.php");
+            echo 'Connected';
+        }
+        catch (PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+
 ?>
 
 <!DOCTYPE html>
@@ -14,14 +55,22 @@
   </head>
   <body>
     <form action="create_account.php" method="POST" class="form-group">
-      <p><label>Student name:
-        <input type="text" name="name" placeholder="Full name" class="form-control"></label>
-        <p><label>Program:<select name="program" class="form-control">
+      <p><label>Student Name
+        <input type="text" name="name" placeholder="Full name" class="form-control" autofocus pattern="[A-Za-z]+\s[A-Za-z]+" required> </label>
+        <p><label>Email
+        <input type="email" name="email" placeholder="Email address" class="form-control" required></label>
+                <p><label>Password
+        <input type="password" name="password" placeholder="Password" class="form-control" required></label>
+
+        <p><label>Program<select name="program" class="form-control">
           <option value="sd">Software Development</option>
           <option value="nw">Networking</option>
         </select></label>
-        <p><label>Estimated graduation date:
-        <input type="text" name="datepicker" placeholder="Select a date" id="datepicker" class="form-control"</label>
+        <p><label>Est. Grad Date
+        <input type="text" name="datepicker" placeholder="Select a date" id="datepicker" class="form-control" required</label>
+        <input id="submit" type="submit" value="Create">
+
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
