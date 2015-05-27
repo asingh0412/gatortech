2dc81257357954d1efdcd310dcec3fecc790031a
asingh0412 <AmaninderS@Amaninders-MacBook-Pro.local>
2015-05-26 17:19:09 -0700

    Summary Page
    Summary page up and running and u can delete user

---
+++ includes/delete_user.php
@@ -0,0 +1,29 @@
+<?php
+    
+    //Turn on error reporting
+    //ini_set('display_errors', 1);
+    //error_reporting(E_ALL);
+    //print_r($_GET);
+    
+    //echo 'I am in php';
+    $id = $_GET['id'];
+    // Database connection information
+    require "db.php";
+    try
+    {
+        // New database connection
+        $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
+        
+        // Login account verification
+        $sql = "DELETE FROM account where email = '$id'";
+        $result = $dbh->query($sql) ;
+        
+        $current_page = $_SERVER['HTTP_REFERER'];
+        header("location: $current_page");
+        
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
--- summary.php
+++ summary.php
@@ -8,55 +8,86 @@
 
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Summary</title>
 
-    <!-- Bootstrap -->
+    <title>Summary</title
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+        <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/profile.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-
+        
+    <link rel="stylesheet" type="text/css" media="all" href="css/index.css">
+        
+        
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.5/css/jquery.dataTables.css">
+		
+    
+    <!-- DataTables-->
+    <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.5/js/jquery.dataTables.js"></script>
+    
+    
+            <script type="text/javascript" charset="utf-8">
+			$(document).ready(function() {
+				$('#example').dataTable();
+			} );
+		</script>
+        
 <body>
+    
+<div class="container">
+
     <h1>Summary</h1>
-    <table>
-        <thead>
-	<tr>
-            <th> Program </th>
-            <th> Status </th>
-            <th> Email</th>
-            <th> Name</th>
-            <th> Estimated Graduation Date</th>
-        </tr>
-       
-        </thead>
-	    <!--<h3>Display the values from DB</h3>-->
-	    <?php
-                 foreach($result as $row){
+    
+        <table id="table-transform" class='table table-bordered table-hover table-striped'>
+            <thead>
+            <tr>
+                <th> Program </th>
+                <th> Status </th>
+                <th> Email </th>
+                <th> Name </th>
+                <th> Estimated Graduation Date</th>
+                <th> Delete </th>
+            </tr>
+            </thead>
+            <tbody>
+                <!--<h3>Display the values from DB</h3>-->
+                <?php
+                     foreach($result as $row){
+                        
                     
-                
-                    echo"<tr class>
-                            <td>{$row[program]}</td>
-                            <td>{$row[status]}</td>
-                            <td>{$row[email]}</td>
-                            <td>{$row[name]}</td>
-                            <td>{$row[egd]}</td>
-                        </tr>";
-                 }
-                ?>
-    </table>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+                        echo"<tr class>
+                                <td>{$row[program]}</td>
+                                <td>{$row[status]}</td>
+                                <td>{$row[email]}</td>
+                                <td>{$row[name]}</td>
+                                <td>{$row[egd]}</td>
+                                <!--<td><button type='button' class='btn btn-default btn-xs' data-toggle='modal' data-target='#myModal'>Delete</button></td>-->
+                               
+                                <td><a href='includes/delete_user.php?id=$row[email]'>
+                                <button type='button' class='btn btn-default btn-xs'>Delete</button></td>
+                            </tr>";
+                     }
+                    ?>
+            
+            </tbody>
+        </table> <!-- End of table-->
+        
+        
+        
+        
+    </div><!-- Enf of Container div-->
+<script>
+  //Script for the data table to look good and searchable.
+    $(document).ready( function () {
+        $('#table-transform').DataTable({"aaSorting": []});
+    });
+</script>
 </body>
 </html>
