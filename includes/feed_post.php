9e715a9735f12a9e47c023f9c14d72e8f973b619
Jim Tryon <tryonjf@gmail.com>
2015-06-01 13:36:23 -0700

    Added redirect to feed

--- includes/feed_post.php
+++ includes/feed_post.php
@@ -32,7 +32,7 @@
             $stmt->bindParam(':hashtag', $_POST['tags']);
             $stmt->execute();
     
-            //header("Location: index.php");
+            header("Location: index.php");
         }
         catch (PDOException $e)
         {
