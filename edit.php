f2aac66bbfa9ed08165f89ae12c1201f87687a49
asingh0412 <AmaninderS@Amaninders-MacBook-Pro.local>
2015-06-03 11:49:53 -0700

    Edit and summary pages
    Made some changes to summary pages and created a edit/update user page.

---
+++ edit.php
@@ -0,0 +1,110 @@
+<?php
+    include('includes/session.php');
+    require "includes/db.php";
+    require "includes/load_edit.php";
+    //require "includes/load_summary.php";
+    
+        //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Edit User</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/profile.css" rel="stylesheet">
+    
+</head>
+
+<body>
+    
+    <div class="container">
+
+        <h1>Edit Accounts</h1>
+        <h4 style="float: right"><a href="index.php">Back to Feed</a></h4>
+        <hr>
+        
+        <form action="includes/edit_user.php" method="POST" class="form-group">
+            
+            <label>Student Name
+                <input type="text" name="name" placeholder="Full name" class="form-control" maxlength="64" autofocus value="<?php echo $name;?>">
+            </label>
+            
+            <p>
+            <label>
+                <input type="hidden" name="id"  placeholder="id" class="form-control" maxlength="64" value="<?=$email?>" >
+            </label>
+            </p>
+            
+            <p>
+            <label>Email
+                <input type="email" name="email" placeholder="Email address" class="form-control" maxlength="64" value="<?=$email?>" >
+            </label>
+            </p>
+            
+            <!-- -->  
+            <?php
+                $selected = $program;
+            ?>
+            <p>
+            <label>Program
+                <select name="program" class="form-control"  >
+                  <option <?php if($selected == 'Advisor'){echo('selected');}?>>Advisor</option>
+                  <option <?php if($selected == 'Software Development'){echo('selected');}?>>Software Development</option>
+                  <option <?php if($selected == 'Networking'){echo('selected');}?>>Networking</option>
+                </select>
+            </label>
+            </p>
+            
+            <p>
+            <label>Est. Grad Date
+                <input type="text" name="egd" placeholder="Select a date" id="datepicker" class="form-control" value="<?=$egd?>">
+            </label>
+            </p>    
+ 
+
+            <button class="btn btn-default" type="submit" id="submit" name="submit-edit" >
+                Update
+            </button>
+        <!--<input name="submit" id="submit" type="submit" value="Create">-->
+        </form>
+    
+    </div><!--Ending containter div-->
+
+    <script>
+        // jQuery Datepicker  
+        $(function()
+        {
+            /* Set to only show the month and year */
+            $('#datepicker').datepicker( {
+                changeMonth: true,
+                changeYear: true,
+                showButtonPanel: true,
+                dateFormat: 'MM yy',
+                onClose: function(dateText, inst) { 
+                    var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+                    var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+                    $(this).datepicker('setDate', new Date(year, month, 1));
+                }
+            });
+        });
+    </script>
+
+
+</body>
+</html>
--- includes/load_summary.php
+++ includes/load_summary.php
@@ -1,75 +1,51 @@
 <?php
-/*try
+
+    if (isset($_POST[user_email]))
+    {
+        try
         {
             // New database connection
             $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
-                /*
+                
             // Login account verification
-            $sql = $dbh->prepare("SELECT picture FROM account WHERE email = '$_SESSION[login_session]'");
+            $sql = $dbh->prepare("SELECT program, status, name, picture, egd, email  FROM account");// WHERE email = '$_POST[user_email]'");
             $sql->execute();
             $result = $sql->fetch();
             
-            $nav_picture = $result['picture'];
+            $program = $result['program'];
+            $status = $result['status'];
+            $name = $result['name'];
+            $picture = $result['picture'];
+            $egd = $result['egd'];
+            $email = $result['email'];
         }
         catch (PDOException $e)
         {
                 echo $e->getMessage();
         }
-  */      
-        if (isset($_POST[user_email]))
+    }
+    else
+    {
+         try
         {
-            try
-            {
-                // New database connection
-                $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
-                    
-                // Login account verification
-                $sql = $dbh->prepare("SELECT program, status, name, picture, egd, email  FROM account");// WHERE email = '$_POST[user_email]'");
-                $sql->execute();
-                $result = $sql->fetch();
+            // New database connection
+            $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
                 
-                $program = $result['program'];
-                $status = $result['status'];
-                $name = $result['name'];
-                $picture = $result['picture'];
-                $egd = $result['egd'];
-                $email = $result['email'];
-    /*
-                $sql2 = $dbh->prepare("SELECT user_name, user_post, post_date, user_email, picture, ID FROM feed WHERE user_email = '$_POST[user_email]' ORDER BY post_date DESC");
-                $sql2->execute();
-                $result2 = $sql2->fetchAll();*/
-            }
-            catch (PDOException $e)
-            {
-                    echo $e->getMessage();
-            }
+            // Login account verification
+            $sql = $dbh->prepare("SELECT program, status, name, picture, egd, email FROM account ");// WHERE email = '$_SESSION[login_session]'");
+            $sql->execute();
+            $result = $sql->fetchAll();
+            
+            $program = $result['program'];
+            $status = $result['status'];
+            $name = $result['name'];
+            $picture = $result['picture'];
+            $egd = $result['egd'];
+            $email = $result['email'];
         }
-        else
+        catch (PDOException $e)
         {
-             try
-            {
-                // New database connection
-                $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
-                    
-                // Login account verification
-                $sql = $dbh->prepare("SELECT program, status, name, picture, egd, email FROM account");// WHERE email = '$_SESSION[login_session]'");
-                $sql->execute();
-                $result = $sql->fetchAll();
-                
-                //$sql = "SELECT program, status, email, name, picture, egd FROM account";
-                //$result = $dbh->query($sql);
-                
-                
-               
-    /*
-                $sql2 = $dbh->prepare("SELECT user_name, user_post, post_date, user_email, picture, ID FROM feed WHERE user_email = '$_SESSION[login_session]' ORDER BY post_date DESC");
-                $sql2->execute();
-                $result2 = $sql2->fetchAll();*/
-                
-            }
-            catch (PDOException $e)
-            {
-                    echo $e->getMessage();
-            }
+                echo $e->getMessage();
         }
+   }
 ?>
