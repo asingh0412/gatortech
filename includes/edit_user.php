f8ef3f5593a1239a38cb698b356912240ed3e42e
asingh0412 <AmaninderS@Amaninders-MacBook-Pro.local>
2015-06-08 11:46:31 -0700

    edited edit_user and edit
    it suppose to validate email

--- includes/edit_user.php
+++ includes/edit_user.php
@@ -4,27 +4,38 @@
     //Turn on error reporting
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
+    print_r($_POST);
     
-    
-    $id = $_POST['id'];
-
     /*
      * Updating the user data as need. 
      */
     
     if (isset($_POST['submit-edit'])) {
         
-        //print_r($_POST);
+        $id = $_POST['id'];
         $program = $_POST['program'];
         $name = $_POST['name'];
-        $email = $_POST['email'];
+        $email1 = $_POST['email'];
         $egd = $_POST['egd'];
-        //echo $name;
+
         try
         {
             // New database connection
             $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
             
+            $email = $dbh->prepare("SELECT email FROM account WHERE email = '$_POST[email]'");
+            $email->execute();
+            
+            // Testing if email already exists
+            //if($id != $email){
+            if($email -> rowCount() > 1){
+                echo 'Email already exists';
+            }
+           else{
+                
+             //   echo "editing user";
+                            // New database connection
+            $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
             // Updating the accout table. 
             $stmt = $dbh->prepare( "UPDATE account
                                     SET program = :program, name =:name, egd = :egd, email= :email
@@ -33,25 +44,20 @@
             $stmt->bindParam(':program', $program, PDO::PARAM_STR);
             $stmt->bindParam(':name', $name, PDO::PARAM_STR);
             $stmt->bindParam(':egd', $egd, PDO::PARAM_STR);
-            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':email', $email1, PDO::PARAM_STR);
             
             $stmt->execute();
-
             //Updating the feed's email 
             $stmt = $dbh->prepare( "UPDATE feed
                                     SET email = :email
                                     WHERE email = '$id'" );
             
-            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':email', $email1, PDO::PARAM_STR);
             $stmt->execute();
             
-            //$sql1 = "DELETE FROM feed where user_email = '$id'";
-            //$result = $dbh->query($sql1) ;
-            
-            //
-            //$current_page = $_SERVER['HTTP_REFERER'];
-            header("location: ../summary.php");
+            //header("location: ../summary.php");
             echo "edited the user";
+          }
             
         }
         catch (PDOException $e)
