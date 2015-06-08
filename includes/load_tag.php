6b1617462e85b1a8a924577e1424d9e2c7f211bc
asingh0412 <AmaninderS@Amaninders-MacBook-Pro.local>
2015-06-08 12:50:44 -0700

    Tag search page
    updated
    load_tag.php
    tab_search.php
    index.php
    
    so tag search is working

---
+++ includes/load_tag.php
@@ -0,0 +1,39 @@
+<?php
+    
+     $hashtag = $_GET['id'];
+    try
+    {
+        // New database connection
+        $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
+            
+        // Login account verification
+        $sql = $dbh->prepare("SELECT picture, status FROM account WHERE email = '$_SESSION[login_session]'");
+        $sql->execute();
+        $result = $sql->fetch();
+        
+        $nav_picture = $result['picture'];
+        $nav_color = $result['status'];
+    }
+    catch (PDOException $e)
+    {
+            echo $e->getMessage();
+    }
+        
+    try
+    {
+        // New database connection
+        $dbh = new PDO("mysql:host=$hostname; dbname=mlant_GT", $username, $password);
+        
+        // Login account verification
+        $sql = $dbh->prepare("SELECT user_name, user_post, post_date, user_email, picture, user_status, ID, hashtag
+                             FROM feed WHERE hashtag = '$hashtag' ORDER BY post_date DESC");
+        $sql->execute();
+        $result = $sql->fetchAll();
+
+        // display all of the rows  
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+    }   
+?>
